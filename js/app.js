9fee07313e1ca5a01f21bbaadb78807ffc7e6270
Tom VanAntwerp <vanantwerp@gmail.com>
2014-08-26 10:35:56 -0400

    Get basic Orbit slider working

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-$(document).foundation();
+$(document).foundation({
+  orbit: {
+    animation: 'slide',
+    timer_speed: 10000,
+    pause_on_hover: true,
+    animation_speed: 500,
+    navigation_arrows: true,
+    slide_number: false,
+    timer: false
+  }
+});
