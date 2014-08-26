3832998141c157eec413ab89af7aa57b668a2c33
Tom VanAntwerp <vanantwerp@gmail.com>
2014-08-26 13:21:06 -0400

    Move custom styling to _settings.scss

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 $(document).foundation({
   orbit: {
     animation: 'slide',
-    timer_speed: 10000,
-    pause_on_hover: true,
+    timer_speed: 5000,
+    pause_on_hover: false,
     animation_speed: 500,
     navigation_arrows: true,
     slide_number: false,
-    timer: false
+    timer: true
   }
 });
